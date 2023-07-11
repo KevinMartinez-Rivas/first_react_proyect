c755be8a21cde5cb71a7f3293e19e9535d58f560
Mhateur <kevmartinez.per@gmail.com>
2023-07-11 12:36:48 -0600

    Header Actualizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto w-full">
                 Seguimiento Pacientes {""}
-                <span className="text-indigo-600">Veterinaria</span>
+                <span className="text-indigo-600">Veterinaria (con GitHub)</span>
             </h1>
         </>
     )
